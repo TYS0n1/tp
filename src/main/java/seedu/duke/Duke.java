05d0527bc662e499f778ad2ec48c4d41018fc218
TYS0n1 <57140494+TYS0n1@users.noreply.github.com>
2020-10-10 20:37:32 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -51,7 +51,7 @@ public class Duke {
             try {
                 String fullCommand = ui.readCommand();
                 Command c = Parser.parse(fullCommand);
-                c.execute(bookmarks, slotList, ui, storage); 
+                c.execute(bookmarks, slots, ui, storage); 
                 isExit = c.isExit();
             } catch (DukeException e) {
                 ui.showErrorMessage(e);
